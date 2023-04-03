bb76a1e70f38cafe4a8c9962ab439a6a7452c1b2
samuel septiano <sammuel.septiano@gmail.com>
2023-04-03 11:43:35 +0700

    initial commit
    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/models/notes_operation.dart';
+import 'package:flutter_provider/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<NotesOperation>(
+      create: (context) => NotesOperation(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
